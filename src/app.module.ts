49e158f0fbfb5995474768decf16ac8d0b219afb
Nishanth-Gobi <nish.professional@gmail.com>
2024-01-23 16:03:37 +0530

    Config setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerService } from './logger/logger.service';
 import { LoggerModule } from './logger/logger.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [LoggerModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.development.env',
+      isGlobal: true, // to avoid importing the CongifModule in every feature module that uses the ConfigService
+    }),
+    LoggerModule,
+  ],
   controllers: [AppController],
   providers: [AppService, LoggerService],
 })
